d2581f6c2127e0a6456f32558484337162a5fc81
bjo <brian88777@gmail.com>
2014-11-15 02:51:57 -0500

    Committing gps + Location tracking

--- Ping/AppDelegate.swift
+++ Ping/AppDelegate.swift
@@ -12,7 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+<<<<<<< Updated upstream
     
+=======
+    var client = MSClient?
+
+>>>>>>> Stashed changes
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
---
+++ Ping/Ping-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Ping-Bridging-Header.h
+//  Ping
+//
+//  Created by Brian Jo on 11/15/14.
+//  Copyright (c) 2014 Cajolise. All rights reserved.
+//
+
+#ifndef Ping_Ping_Bridging_Header_h
+#define Ping_Ping_Bridging_Header_h
+
+#import <WindowsAzureMobileServices/WindowsAzureMobileServices.h>
+
+#endif
